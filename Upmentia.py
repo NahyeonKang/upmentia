d39bfd4ee4c59c5d351261ca2802be175879fbf6
NAHYEON KANG <ooroosa1130@gmail.com>
2023-09-13 22:07:56 +0900

    Update Upmentia.py

--- Upmentia.py
+++ Upmentia.py
@@ -4,23 +4,14 @@ from audiorecorder import audiorecorder
 import requests
 import io
 from PIL import Image
-from backend import app
-import uvicorn
 
 st.set_page_config(page_title="Upmentia", page_icon=":thumbsup:", initial_sidebar_state="collapsed")
 
 if "newsession" not in st.session_state:
     st.session_state["newsession"] = True
-    
-if __name__ == "__main__":
-    if st.session_state["newsession"]:
-        try:
-            uvicorn.run(app, host="0.0.0.0", port=8000)
-            st.session_state[newsession] = False
-            pass
-        except:
-            pass
-            
+else:
+    st.session_state["newsession"] = False
+
 with st.container():
     col1, col2 = st.columns([1,2])
     with col1:
@@ -35,3 +26,12 @@ with st.container():
 st.markdown('''
 #### :ear:어르신의 추억을 들려주세요!  
 #### :point_left:어떤 이야기를 들려주고 싶으신가요? 왼쪽에서 주제를 선택해보세요''')
+
+
+if __name__ == "__main__":
+    if st.session_state["newsession"]:
+        try:
+            from backend import init
+            
+        except:
+            pass
